f70fea07f225786231eec851f0c030f67074e5e2
abdullah-turgut <abdullah.turgut.94@gmail.com>
2024-01-06 01:02:06 +0300

    Update Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,18 +2,32 @@ import { useEffect, useState } from 'react';
 import MultiSelect from './MultiSelect';
 import { Character } from '../../types/types';
 import { fetchCharacters } from '../../services/api';
+import { Loader2 } from 'lucide-react';
 
 const Main = () => {
   const [data, setData] = useState<Character[] | []>([]);
+  const [loaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const characters = await fetchCharacters();
       setData(characters);
+      setIsLoaded(true);
     };
 
     fetchData();
   }, []);
+
+  if (!loaded) {
+    return (
+      <main className="pt-24 lg:pt-32 pb-16  h-screen flex justify-center items-start px-5">
+        <div className="flex items-center gap-x-5">
+          <p>Retrieving data from API...</p>
+          <Loader2 className="animate-spin" />
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main className="pt-24 lg:pt-32 pb-16  h-screen flex justify-center items-start px-5">
